6a7b3717f81634ffb1cb7bea261dedd60cd73842
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-06-18 10:11:52 -0400

    Create platform.cpp

---
+++ platform.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+      // Create Window Class
+      WNDCLASS window_class = {};
+      window_class.style = CS_HREDRAW | CS_VREDRAW;
+      window_class.lpszClassName = "Game Window Class";
+      window_class.lpfnWndProc
+      // Register Classs
+      
+      // Create Window
+}
