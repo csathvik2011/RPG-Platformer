38d7c8461609941d0881a8293edd42e5ebd0058b
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-06-19 18:30:28 -0400

    Update renderer.cpp

--- renderer.cpp
+++ renderer.cpp
@@ -1,3 +1,8 @@
 void render_background() {
-  
+   unsigned int* pixel = (unsigned int)buffer_memory;
+           for (int y = 0; y < buffer_height; y++) {
+                 for (int x = 0; x < buffer_width; y++) {
+                       *pixel++ = 0xff00ff;
+                 }
+           }
 }
