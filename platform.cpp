712b16d7f7052ee17e19bd5815e2b32e0de137d1
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-06-19 13:32:12 -0400

    Update platform.cpp

--- platform.cpp
+++ platform.cpp
@@ -1,10 +1,14 @@
 #include <windows.h>
 
 bool running = true;
+struct Render_State {
+      int height, width;
+      void* memory
+      
+      BITMAPINFO bitmapinfo;
+};
 
-void* buffer_memory;
-int buffer_width;
-int buffer_height;
+Render_State render_state;
 
 LRESULT CALLBACK WindowProc window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
       LRESULT result = 0;
@@ -25,6 +29,14 @@ LRESULT CALLBACK WindowProc window_callback(HWND hwnd, UINT uMsg, WPARAM wParam,
                   
                   if (buffer_memory) VirtualFree(buffer_memory, 0, MEM_RELEASE);
                   buffer_memory = VirtualAlloc(0, buffer_size, MEM COMMIT | MEM_RESERVE, PAGE_READWRITE);
+                 
+                  buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader)
+                  buffer_bitmap_info.bmiHeader.biWidth = buffer_width;
+                  buffer_bitmap_info.bmiHeader.biHeight = buffer_height;
+                  buffer_bitmap_info.bmiHeader.biPlanes = 1;
+                  buffer_bitmap_info.bmiHeader.biBitCount = 32;
+                  buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
+                 
             } break;
                   
             default: {
@@ -44,6 +56,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
       RegisterClass(&window_class);
       // Create Window
     HWND window = CreateWindow(window_class.lpszClassName, "My First Game!", WS_OVERLAPPEWINDOW | WS_Visible, CWUSEDEFAULT, CWUSEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+    HDC hdc = GetDC(window);
           while (running) {
             // Input
             MSG message;
@@ -52,6 +65,15 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
                   DispatchMessage(&message)
             }
            // Simulate
-         
+           unsigned int* pixel = (unsigned int)buffer_memory;
+           for (int y = 0; y < buffer_height; y++) {
+                 for (int x = 0; x < buffer_width; y++) {
+                       *pixel++ = 0xff00ff;
+                 }
+           }
           // Render
+          StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width, buffer_height, buffer_memory, &buffer_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+      }
+
+      
 }
