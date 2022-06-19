5fab7a6e7f06fe069b64f71bb372fdf13115cab6
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-06-19 18:44:10 -0400

    Update renderer.cpp

--- renderer.cpp
+++ renderer.cpp
@@ -6,3 +6,21 @@ void render_background() {
                  }
            }
 }
+
+void clear_screen(unsigned int color) {
+   unsigned int* pixel = (unsigned int)buffer_memory;
+           for (int y = 0; y < buffer_height; y++) {
+                 for (int x = 0; x < buffer_width; y++) {
+                       *pixel++ = color;
+                 }
+           }
+}
+void draw_rect(int x0, int y0, int x1, int y1, unsigned int color) {
+
+   for (int y = 0; y < buffer_height; y++) {
+      unsigned int* pixel = (unsigned int)buffer_memory + x0 + y*buffer_width;
+                 for (int x = 0; x < buffer_width; y++) {
+                       *pixel++ = 0xff00ff;
+                 }
+           }
+}
