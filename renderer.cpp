e45e23f1927bc6e1db72dfad383c90a996a27b26
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-06-20 20:04:27 -0400

    Update renderer.cpp

--- renderer.cpp
+++ renderer.cpp
@@ -15,8 +15,15 @@ void clear_screen(unsigned int color) {
                  }
            }
 }
+
 void draw_rect(int x0, int y0, int x1, int y1, unsigned int color) {
 
+   x0 = clamp(0, x0, buffer_width)
+   x1 = clamp(0, x1, buffer_width)
+   y0 = clamp(0, y0, buffer_height)
+   y1 = clamp(0, y1, buffer_height)
+   
+   
    for (int y = 0; y < buffer_height; y++) {
       unsigned int* pixel = (unsigned int)buffer_memory + x0 + y*buffer_width;
                  for (int x = 0; x < buffer_width; y++) {
